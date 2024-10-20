5d6ccfb662d07255d2ae0c174ca51fade05e934e
nextSaimon <saimonss5432@gmail.com>
2024-10-20 17:30:54 +0600

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,84 +1,57 @@
 import React from "react";
 
 const Contact = () => {
+    const Projects = [
+        {
+            title: "Project One",
+            img: "./assets/project-1.png",
+            github: "#",
+            demo: "#"
+        },
+        {
+            title: "Project One",
+            img: "./assets/project-1.png",
+            github: "#",
+            demo: "#"
+        }, {
+            title: "Project One",
+            img: "./assets/project-1.png",
+            github: "#",
+            demo: "#"
+        }
+    ]
     return (
         <section id="projects">
             <p className="section__text__p1">Browse My Recent</p>
             <h1 className="title">Projects</h1>
             <div className="experience-details-container">
                 <div className="about-containers">
-                    <div className="details-container color-container">
-                        <div className="article-container">
-                            <img
-                                src="./assets/project-1.png"
-                                alt="Project 1"
-                                className="project-img"
-                            />
+                    {Projects.map((data) => (
+                        <div className="details-container color-container">
+                            <div className="article-container" >
+                                <img
+                                    src={data.img}
+                                    alt={data.title}
+                                    className="project-img"
+                                />
+                            </div>
+                            <h2 className="experience-sub-title project-title">{data.title}</h2>
+                            <div className="btn-container">
+                                <button
+                                    className="btn btn-color-2 project-btn"
+                                    onClick={data.github}
+                                >
+                                    Github
+                                </button>
+                                <button
+                                    className="btn btn-color-2 project-btn"
+                                    onClick={data.demo}
+                                >
+                                    Live Demo
+                                </button>
+                            </div>
                         </div>
-                        <h2 className="experience-sub-title project-title">Project One</h2>
-                        <div className="btn-container">
-                            <button
-                                className="btn btn-color-2 project-btn"
-                                onclick="location.href='https://github.com/'"
-                            >
-                                Github
-                            </button>
-                            <button
-                                className="btn btn-color-2 project-btn"
-                                onclick="location.href='https://github.com/'"
-                            >
-                                Live Demo
-                            </button>
-                        </div>
-                    </div>
-                    <div className="details-container color-container">
-                        <div className="article-container">
-                            <img
-                                src="./assets/project-2.png"
-                                alt="Project 2"
-                                className="project-img"
-                            />
-                        </div>
-                        <h2 className="experience-sub-title project-title">Project Two</h2>
-                        <div className="btn-container">
-                            <button
-                                className="btn btn-color-2 project-btn"
-                                onclick="location.href='https://github.com/'"
-                            >
-                                Github
-                            </button>
-                            <button
-                                className="btn btn-color-2 project-btn"
-                                onclick="location.href='https://github.com/'"
-                            >
-                                Live Demo
-                            </button>
-                        </div>
-                    </div>
-                    <div className="details-container color-container">
-                        <div className="article-container">
-                            <img
-                                src="./assets/project-3.png"
-                                alt="Project 3"
-                                className="project-img"
-                            />
-                        </div>
-                        <h2 className="experience-sub-title project-title">Project Three</h2>
-                        <div className="btn-container">
-                            <button
-                                className="btn btn-color-2 project-btn"
-                                onclick="location.href='https://github.com/'"
-                            >
-                                Github
-                            </button>
-                            <button
-                                className="btn btn-color-2 project-btn"
-                                onclick="location.href='https://github.com/'"
-                            >
-                                Live Demo
-                            </button>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
