1f0bafb7530c026eb8dedc8e12efc09bae488e63
Saimon <saimonss5432@gmail.com>
2025-07-04 20:41:32 +0600

    update

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import localFont from "next/font/local";
 import "./assets/css/style.css";
 import "./assets/css/mediaqueries.css";
 import "./globals.css";
-import Navbar from "./components/Navbar";
 import Head from "next/head";
 import Footer from "./components/Footer";
 
@@ -40,9 +39,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <Navbar /> */}
         {children}
-        <Footer />
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Experience />
       <Contact />
+      <Footer />
     </>
   );
 }
