c67caeb183b4061e68bf0093073236c3d673c27b
Saimon <saimonss5432@gmail.com>
2025-07-06 10:34:39 +0600

    metadata update

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import localFont from "next/font/local";
 import "./assets/css/style.css";
 import "./assets/css/mediaqueries.css";
 import "./globals.css";
-import Head from "next/head";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -24,18 +23,14 @@ export const metadata = {
     "Welcome to Saimon's portfolio! Discover my skills in web development, design, and programming. Explore my projects and learn more about my work.",
   keywords:
     "Saimon, web developer, software developer, front-end development, full-stack development, programming, coding, portfolio, web projects, JavaScript, React, HTML, CSS, JavaScript, NextJs, React, TailwindCSS, Bootstrap, responsive design, developer portfolio, UHSC, SSC 2023, HSC 2025",
+  other: {
+    "google-site-verification": "3P2pzOaMiJRrHdOKR7EbqByO3g1OlCgpzsrcHxu0mM8",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="google-site-verification"
-          content="zdUPNuU8cl5tovxUSu2ArF1BL3PuKZ1T3_yIra13vxg"
-        />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
