8e502434043cb04ab963496698cbbf5a7a8c4564
Saimon <saimonss5432@gmail.com>
2025-07-06 15:10:46 +0600

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+  // for statig
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
+};
 
 export default nextConfig;
